cc85c4748e943add6033b824214a30a9281aecab
Facucarbon <facundocarbon2015@gmail.com>
2025-06-13 15:21:32 -0300

    Remove export global css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import "./index.css";
 export { HeaderMobile } from "./components/header-mobile/Header-mobile";
 export { HeaderDesktop } from "./components/header-desktop/Header-desktop";
 export { Layout } from "./components/layout/Layout";
